f740f9921dfb00bc9517b5d0bb5a58724877a143
nuxZERO <natthawut1991@gmail.com>
2015-02-09 15:40:06 +0700

    Publish to jCenter

--- sample/src/main/java/me/cafecode/android/samples/espresso/MainActivity.java
+++ sample/src/main/java/me/cafecode/android/samples/espresso/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import me.cafecode.android.libraries.espresso.Espresso;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Espresso.hello(this);
     }
 
     @Override
